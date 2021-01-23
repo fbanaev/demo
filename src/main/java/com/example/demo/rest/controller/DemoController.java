40507f5ef8853dcd66906cbc732c70d7d2945e0e
fbanaev <streptoch@gmail.com>
2021-01-23 04:32:48 +0300

    initial

---
+++ src/main/java/com/example/demo/rest/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Well done";
+    }
+}
