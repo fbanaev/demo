d8a4c8cca86774bfadc6ec6fd6c552fd5a7aad5d
FSBanaev <FSBanaev@sberbank.ru>
2021-01-24 01:57:16 +0300

    dockerfile fix

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,9 @@
 pipeline {
-    agent { docker { image 'maven:3.3.3' } }
+    agent { image 'openjdk:11' }
     stages {
-        stage('build') {
+        stage('Build') { 
             steps {
-                sh 'mvn --version'
+                sh 'mvn -B -DskipTests clean package' 
             }
         }
     }
--- src/main/java/com/example/demo/rest/controller/DemoController.java
+++ src/main/java/com/example/demo/rest/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoController {
     @GetMapping("/")
     public String test() {
         return "Well done";
+    }
+
+    @GetMapping("/")
+    public String getText(@RequestAttribute("text") String text) {
+        return text;
     }
 }
